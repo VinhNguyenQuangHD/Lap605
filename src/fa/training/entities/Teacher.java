853944035271c024091d2be8de3e4955f409cf9c
VinhNguyenQuangHD <61536993+VinhNguyenQuangHD@users.noreply.github.com>
2022-05-13 22:19:44 +0700

    add

---
+++ src/fa/training/entities/Teacher.java
@@ -0,0 +1,37 @@
+package fa.training.entities;
+
+public abstract class Teacher {
+    private String designation;
+    private String collegeName;
+
+	    public Teacher() {
+	    }
+
+	    public Teacher(String designation, String collegename) {
+	        super();
+	        this.designation = designation;
+	        this.collegeName = collegename;
+	    }
+
+	    public String getDesignation() {
+	        return designation;
+	    }
+
+	    public void setDesignation(String designation) {
+	        this.designation = designation;
+	    }
+
+	    public String getCollegename() {
+	        return collegeName;
+	    }
+
+	    public void setCollegename(String collegename) {
+	        this.collegeName = collegename;
+	    }
+
+	    public abstract void teach();
+
+	    public void teach(int duration) {
+	        System.out.println("Teaching in " + duration + " minutes");
+	    }
+}
